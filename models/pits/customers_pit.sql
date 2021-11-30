d4224559176195c46a61cc13c9847ce3cdfbb0ac
ProkMar <prokmar@mail.ru>
2021-11-30 22:25:41 +0300

    Срез последних по двум сателитам.

--- models/pits/customers_pit.sql
+++ models/pits/customers_pit.sql
@@ -5,15 +5,15 @@
 }}
 
 select	
-	scd.customer_hashdiff as scd_customer_hashdiff,
-	null as scc_customer_hashdiff,
-    max(scd.load_date) as scd_load_date
+	scd.customer_pk as scd_customer_pk,
+	null as scc_customer_pk,
+    max(scd.load_date) as load_date
 from {{ref('sat_customer_details')}} scd
 group by 1,2
 union all
 select	
 	null,
-	scc.customer_hashdiff,
+	scc.customer_pk,
     max(scc.load_date)
 from {{ref('sat_customers_crm')}} scc
 group by 1,2
--- models/reports/customers_with_crm.sql
+++ models/reports/customers_with_crm.sql
@@ -4,22 +4,46 @@
     )
 }}
 
+select
+
+    t.customer_pk,
+    t.customer_key,
+    t.first_name,
+    t.last_name,
+    t.email,
+    t.country,
+    t.age
+from
+(select 
+	hc.customer_pk, 
+	hc.customer_key, 	
+	coalesce(scd.first_name, '') as first_name,
+	coalesce(scd.last_name, '') as last_name,
+	coalesce(scd.email, '') as email,
+    null as country,
+	null as age
+from {{ref('hub_customer')}} hc
+left join {{ref('sat_customer_details')}} scd 
+on hc.customer_pk = scd.customer_pk
+    inner join {{ref('customers_pit')}} as cp_scd
+    on scd.customer_pk = cp_scd.scd_customer_pk
+    and scd.load_date = cp_scd.load_date
+
+union all
+
 select 
 	hc.customer_pk, 
 	hc.customer_key, 	
-	scd.first_name,
-	scd.last_name,
-	scd.email,
-    scc.country,
-	scc.age
+	null,
+	null,
+	null,
+    coalesce(scc.country, ''),
+	coalesce(scc.age, 0)
 from {{ref('hub_customer')}} hc
-inner join {{ref('sat_customer_details')}} scd 
-on hc.customer_pk = scd.customer_pk
-    inner join {{ref('customers_pit')}} as cp_scd
-    on scd.customer_hashdiff = cp_scd.scd_customer_hashdiff
-
-inner join {{ref('sat_customers_crm')}} scc 
+left join {{ref('sat_customers_crm')}} scc 
 on hc.customer_pk = scc.customer_pk 
     inner join {{ref('customers_pit')}} as cp_scc
-    on scc.customer_hashdiff = cp_scc.scc_customer_hashdiff
+    on scc.customer_pk = cp_scc.scc_customer_pk
+    and scc.load_date = cp_scc.load_date) as t
 
+group by 1,2,3,4,5,6,7
